cb79b94c5a49847cc36a8bfbc5a8acd560c4f2fe
zt-a <zt20061113@gmail.com>
2024-11-24 21:00:09 +0600

    edit send message

--- app/main/views.py
+++ app/main/views.py
@@ -8,35 +8,27 @@ from .utils import send_telegram_notification
 
 def home(request):
     if request.method == 'POST':
-        # Если вы используете Django Form, используйте её для валидации данных
         form = OrderForm(request.POST)
 
         if form.is_valid():
-            # Если форма валидна, сохраняем данные в базе данных
             order = form.save()
+            message_text = f"Новый заказ от {order.name}!\nТема: {order.topic}\nОписание: {order.description}\nНомер: {order.phone_number}\nТип: {order.document_type}\nСрок: {order.deadline}!"
 
-            send_mail(
-                'Новый заказ!',
-                f'Заказ от {order.name}:\nТема: {order.topic}\nОписание: {order.description}',
+            send_mail(f'Заказ на {order.document_type}', message_text,
                 settings.DEFAULT_FROM_EMAIL,  # От кого
                 ['zt20061113@gmail.com', 'adulovbilal001@gmail.com', 'nazilova001@gmail.com', 'tunukaijanyshova@gmail.com'],
                 fail_silently=False,
             )
             
-            telegram_message = f"Новый заказ от {order.name}!\nТема: {order.topic}\nОписание: {order.description}"
-            send_telegram_notification(telegram_message)
+            send_telegram_notification(message_text)
 
-            # Выводим сообщение об успешной отправке
             messages.success(request, 'Заказ успешно создан!')
 
-            # Перенаправляем на страницу успеха
             return redirect('success_page')  # Укажите свой путь для успешной отправки
         else:
-            # Если форма не валидна, возвращаем пользователю ошибки
-            messages.error(request, 'Пожалуйста, исправьте ошибки в форме и заполните пустые поля.')
+            messages.error(request, f'Пожалуйста, исправьте ошибки в форме и заполните пустые поля: {form.errors}')
 
     else:
-        # Если метод GET, создаем пустую форму
         form = OrderForm()
 
     return render(request, 'main/home.html', {'form': form})
